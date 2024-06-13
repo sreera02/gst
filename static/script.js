424fa82eec743f14f339bfa28d1b44d8054dc4d5
sreera02 <srr_developer@protonmail.com>
2024-06-12 23:58:45 -0400

    MAJOR CHANGE #1

--- static/script.js
+++ static/script.js
@@ -1,41 +1,6 @@
-'use strict';
+const grid = document.querySelector(".nav-collapsed");
+const btn = document.querySelector(".menu");
 
-const addEventOnElem = function (elem, type, callback) {
-    if (elem.length > 1){
-        for (let i = 0; i < elem.length; i++) {
-            elem[i].addEventListener(type, callback);
-        }
-    } else {
-        elem.addEventListener(type, callback);
-    }
-}
-
-const navbar = document.querySelector("[data-navbar]");
-const navToggler = document.querySelectorAll("[data-nav-toggler]");
-const overlay = document.querySelector("[data-overlay]");
-const toggleNavbar = function () {
-    navbar.classList.toggle("active");
-    overlay.classList.toggle("active");
-}
-
-addEventOnElem(navToggler, "click", toggleNavbar);
-
-const navLinks = document.querySelectorAll("[data-nav-link]");
-const closeNavbar = function () {
-    navbar.classList.remove("active");
-    overlay.classList.remove("active");
-}
-
-addEventOnElem(navLinks, "click", closeNavbar);
-
-const header = document.querySelector("[data-header]");
-
-const headerActive = function () {
-    if (window.scrollY > 100) {
-        header.classList.add("active");
-    } else {
-        header.classList.remove("active");
-    }
-}
-
-addEventOnElem(window, "scroll", headerActive)
+btn.addEventListener("click", () => {
+    grid.classList.toggle("active");
+})
